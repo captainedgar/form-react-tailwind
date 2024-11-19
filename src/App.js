1080e8f152e9f7f2a5e382dbb630fdb8e87ab011
captainedgar <45235580+captainedgar@users.noreply.github.com>
2024-11-19 10:52:58 -0400

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -25,17 +25,24 @@ const handleChange = (e)=>{
 e.preventDefault()
 console.log(formdate);
 
-setFormadate('')
+setFormadate({
+  name: '',
+  lastname: '',
+  mail: '',
+  message: '',
+  checkbox: '',
+  option: ''
+})
   }
   return (
     <>
   <div className="bg-customGreen flex items-center justify-center h-screen">
   <form onSubmit={infoform}>
-    <div className="max-w-[720px] p-6 bg-white grid grid-cols-1 gap-4 items-center rounded-lg h-128">
-      <h2 className="text-lg font-bold">Contact US</h2>
-      
-      {/* Campos First Name y Last Name en paralelo */}
-      <div className="grid grid-cols-2 gap-4">
+    <div className="max-w-[720px] p-6 bg-white grid grid-cols-1 gap-4 items-center rounded-lg">
+      <h2 className="text-[1.5rem] font-bold">Contact US</h2>
+
+      {/* Campos First Name y Last Name */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div className="flex flex-col">
           <label htmlFor="firstName">First Name <span className="text-botonColor">*</span></label>
           <input
@@ -59,11 +66,11 @@ setFormadate('')
           />
         </div>
       </div>
-      
+
       {/* Campo Email */}
       <div className="flex flex-col">
         <label htmlFor="emailAdress">Email Address <span className="text-botonColor">*</span></label>
-        <input 
+        <input
           type="email"
           id="mail"
           name="mail"
@@ -72,34 +79,34 @@ setFormadate('')
           className="border-2 rounded p-2"
         />
       </div>
-      
+
       {/* Campo Query Type */}
-      <label htmlFor="queryType">Query Type <span className="text-botonColor">*</span></label>
-      <div className="flex space-x-4">
-        <div className="" >
-        <input
-          type="radio"
-          onChange={handleChange}
-          value="General Enquiry"
-          name="option"
-          id="option2"
-         className="border-custom"
-        />
+      <label htmlFor="queryType" className="pl-2">Query Type <span className="text-botonColor">*</span></label>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="flex items-center gap-x-2 border-2 rounded p-2">
+          <input
+            type="radio"
+            onChange={handleChange}
+            value="General Enquiry"
+            name="option"
+            id="option2"
+            checked={formdate.option === "General Enquiry"}
+          />
+          <label htmlFor="generalEnquiry" className="pl-1">General Enquiry</label>
         </div>
-        <div>
-        <label htmlFor="generalEnquiry">General Enquiry</label>
-        
-        <input
-          type="radio"
-          onChange={handleChange}
-          value="Support Request"
-          name="option"
-          id="option1"
-        />
-        <label htmlFor="supportRequest">Support Request</label>
+        <div className="flex items-center gap-x-2 border-2 rounded p-2">
+          <input
+            type="radio"
+            onChange={handleChange}
+            value="Support Request"
+            name="option"
+            id="option1"
+            checked={formdate.option === "Support Request"}
+          />
+          <label htmlFor="supportRequest" className="pl-1">Support Request</label>
         </div>
       </div>
-      
+
       {/* Campo Message */}
       <div className="flex flex-col">
         <label htmlFor="message">Message <span className="text-botonColor">*</span></label>
@@ -113,29 +120,31 @@ setFormadate('')
           className="border-2 rounded p-2"
         ></textarea>
       </div>
-      
+
       {/* Consent */}
-      <div className="flex space-x-4 items-center">
+      <div className="flex items-start flex-col sm:flex-row sm:space-x-4">
         <input
           type="checkbox"
           id="checkbox"
           name="checkbox"
           value="Checked"
+          checked={formdate.checkbox === "Checked"}
           onChange={handleChange}
         />
-        <label htmlFor="consent">I consent to being contacted by the team <span className="text-botonColor">*</span></label>
+        <label htmlFor="consent" className="sm:pl-2">I consent to being contacted by the team <span className="text-botonColor">*</span></label>
       </div>
-      
+
       {/* Submit Button */}
       <button
         type="submit"
-        className="bg-botonColor rounded-md p-2 w-full text-white"
+        className="bg-botonColor rounded-md p-2 w-full text-white max-w-full"
       >
         Submit
       </button>
     </div>
   </form>
 </div>
+
 
 
     </>
