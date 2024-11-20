2ca7fa17f5362a5707ad643d5c7929b1bfb76747
captainedgar <45235580+captainedgar@users.noreply.github.com>
2024-11-20 15:46:51 -0400

    Los campos requeridos con diseño

--- src/App.js
+++ src/App.js
@@ -1,152 +1,129 @@
-import { useState } from "react";
-
-
+import { useForm } from "react-hook-form";
+import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset, // Para resetear el formulario
+  } = useForm();
 
-const [formdate, setFormadate] = useState({
-  name: '',
-  lastname: '',
-  mail:'',
-  message: '',
-  checkbox: '',
-  option: ''
- 
-})
-const handleChange = (e)=>{
-  const {name, value} = e.target;
+  // Función que maneja el submit del formulario
+  const infoform = (data) => {
+    console.log(data);
 
-  setFormadate({
-   ...formdate, [name]: value
-   })
-}
-  const infoform=(e)=>{
-   
-e.preventDefault()
-console.log(formdate);
+    // Resetea el formulario después de submit
+    reset();
+  };
 
-setFormadate({
-  name: '',
-  lastname: '',
-  mail: '',
-  message: '',
-  checkbox: '',
-  option: ''
-})
-  }
   return (
     <>
-  <div className="bg-customGreen flex items-center justify-center h-screen">
-  <form onSubmit={infoform}>
-    <div className="max-w-[720px] p-6 bg-white grid grid-cols-1 gap-4 items-center rounded-lg">
-      <h2 className="text-[1.5rem] font-bold">Contact US</h2>
+      <div className="bg-customGreen flex items-center justify-center h-screen">
+        <form onSubmit={handleSubmit(infoform)}>
+          <div className="max-w-[720px] p-6 bg-white grid grid-cols-1 gap-4 items-center rounded-lg">
+            <h2 className="text-[1.5rem] font-bold">Contact US</h2>
 
-      {/* Campos First Name y Last Name */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className="flex flex-col">
-          <label htmlFor="firstName">First Name <span className="text-botonColor">*</span></label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formdate.name}
-            onChange={handleChange}
-            className="border-2 rounded p-2"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="lastName">Last Name <span className="text-botonColor">*</span></label>
-          <input
-            type="text"
-            id="lastname"
-            name="lastname"
-            value={formdate.lastname}
-            onChange={handleChange}
-            className="border-2 rounded p-2"
-          />
-        </div>
+            {/* Campos First Name y Last Name */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <div className="flex flex-col">
+                <label htmlFor="firstName">
+                  First Name <span className="text-botonColor">*</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: "Name is required" })}
+                  className={`border-2 p-2 rounded ${errors.name ? "border-red-500" : "border-gray-300"}`}
+                />
+                {errors.name && (
+                  <p className="text-red-500 flex items-center">
+                    <ExclamationCircleIcon className="w-5 h-5 mr-2" /> {/* Ícono */}
+                    {errors.name.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="lastName">
+                  Last Name <span className="text-botonColor">*</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("lastname", { required: "Last name is required" })}
+                  className={`border-2 p-2 rounded ${errors.lastname ? "border-red-500" : "border-gray-300"}`}
+                />
+                {errors.lastname && (
+                  <p className="text-red-500 flex items-center">
+                    <ExclamationCircleIcon className="w-5 h-5 mr-2" /> {/* Ícono */}
+                    {errors.lastname.message}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            {/* Campo Email */}
+            <div className="flex flex-col">
+              <label htmlFor="emailAdress">
+                Email Address <span className="text-botonColor">*</span>
+              </label>
+              <input
+                type="email"
+                {...register("mail", { required: "Email is required" })}
+                className={`border-2 p-2 rounded ${errors.mail ? "border-red-500" : "border-gray-300"}`}
+              />
+              {errors.mail && (
+                <p className="text-red-500 flex items-center">
+                  <ExclamationCircleIcon className="w-5 h-5 mr-2" /> {/* Ícono */}
+                  {errors.mail.message}
+                </p>
+              )}
+            </div>
+
+            {/* Campo Message */}
+            <div className="flex flex-col">
+              <label htmlFor="message">
+                Message <span className="text-botonColor">*</span>
+              </label>
+              <textarea
+                {...register("message", { required: "Message is required" })}
+                rows="3"
+                className={`border-2 p-2 rounded ${errors.message ? "border-red-500" : "border-gray-300"}`}
+              ></textarea>
+              {errors.message && (
+                <p className="text-red-500 flex items-center">
+                  <ExclamationCircleIcon className="w-5 h-5 mr-2" /> {/* Ícono */}
+                  {errors.message.message}
+                </p>
+              )}
+            </div>
+
+            {/* Consent */}
+            <div className="flex items-start flex-col sm:flex-row sm:space-x-4">
+              <input
+                type="checkbox"
+                {...register("checkbox", { required: "Consent is required" })}
+              />
+              <label htmlFor="consent" className="sm:pl-2">
+                I consent to being contacted by the team{" "}
+                <span className="text-botonColor">*</span>
+              </label>
+            </div>
+            {errors.checkbox && (
+              <p className="text-red-500 flex items-center">
+                <ExclamationCircleIcon className="w-5 h-5 mr-2" /> {/* Ícono */}
+                {errors.checkbox.message}
+              </p>
+            )}
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="bg-botonColor rounded-md p-2 w-full text-white max-w-full"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-
-      {/* Campo Email */}
-      <div className="flex flex-col">
-        <label htmlFor="emailAdress">Email Address <span className="text-botonColor">*</span></label>
-        <input
-          type="email"
-          id="mail"
-          name="mail"
-          value={formdate.mail}
-          onChange={handleChange}
-          className="border-2 rounded p-2"
-        />
-      </div>
-
-      {/* Campo Query Type */}
-      <label htmlFor="queryType" className="pl-2">Query Type <span className="text-botonColor">*</span></label>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className="flex items-center gap-x-2 border-2 rounded p-2">
-          <input
-            type="radio"
-            onChange={handleChange}
-            value="General Enquiry"
-            name="option"
-            id="option2"
-            checked={formdate.option === "General Enquiry"}
-          />
-          <label htmlFor="generalEnquiry" className="pl-1">General Enquiry</label>
-        </div>
-        <div className="flex items-center gap-x-2 border-2 rounded p-2">
-          <input
-            type="radio"
-            onChange={handleChange}
-            value="Support Request"
-            name="option"
-            id="option1"
-            checked={formdate.option === "Support Request"}
-          />
-          <label htmlFor="supportRequest" className="pl-1">Support Request</label>
-        </div>
-      </div>
-
-      {/* Campo Message */}
-      <div className="flex flex-col">
-        <label htmlFor="message">Message <span className="text-botonColor">*</span></label>
-        <textarea
-          id="message"
-          name="message"
-          rows="3"
-          cols="10"
-          value={formdate.message}
-          onChange={handleChange}
-          className="border-2 rounded p-2"
-        ></textarea>
-      </div>
-
-      {/* Consent */}
-      <div className="flex items-start flex-col sm:flex-row sm:space-x-4">
-        <input
-          type="checkbox"
-          id="checkbox"
-          name="checkbox"
-          value="Checked"
-          checked={formdate.checkbox === "Checked"}
-          onChange={handleChange}
-        />
-        <label htmlFor="consent" className="sm:pl-2">I consent to being contacted by the team <span className="text-botonColor">*</span></label>
-      </div>
-
-      {/* Submit Button */}
-      <button
-        type="submit"
-        className="bg-botonColor rounded-md p-2 w-full text-white max-w-full"
-      >
-        Submit
-      </button>
-    </div>
-  </form>
-</div>
-
-
-
     </>
   );
 }
